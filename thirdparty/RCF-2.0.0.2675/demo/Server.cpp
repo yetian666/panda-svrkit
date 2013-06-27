9140fad51b03fc23370832732bc24a3428942e7d
haihuan <haihuan.yu@gmail.com>
2013-06-27 13:44:54 +0800

    ci Signed-off-by: haihuan <haihuan.yu@gmail.com>

---
+++ thirdparty/RCF-2.0.0.2675/demo/Server.cpp
@@ -0,0 +1,40 @@
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <RCF/RCF.hpp>
+
+#include "MyService.hpp"
+
+class MyServiceImpl
+{
+public:
+    // Reverses the order of strings in the vector.
+    void reverse(std::vector<std::string> &v)
+    {
+        std::cout << "Reversing a vector of strings...\n";
+        std::vector<std::string> w;
+        std::copy(v.rbegin(), v.rend(), std::back_inserter(w));
+        v.swap(w);
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    RCF::RcfInitDeinit rcfInit;
+
+    // Start a TCP server on port 50001, and expose MyServiceImpl.
+    MyServiceImpl myServiceImpl;
+    RCF::RcfServer server( RCF::TcpEndpoint("0.0.0.0", 50001) );
+    server.bind<MyService>(myServiceImpl);
+    RCF::ThreadPoolPtr tpPtr( new RCF::ThreadPool(atoi(argv[1])) );
+	server.setThreadPool(tpPtr);
+    server.start();
+
+    std::cout << "Press Enter to exit..." << std::endl;
+    std::cin.get();
+
+    return 0;
+}
